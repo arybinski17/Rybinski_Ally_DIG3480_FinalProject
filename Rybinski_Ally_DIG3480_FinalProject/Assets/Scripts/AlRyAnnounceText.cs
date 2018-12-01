cfbcc0f049152c80b850f3b81ceba9cd40c41c2c
Ally <alexandriarybinski@gmail.com>
2018-12-01 01:45:51 -0500

    Final Project

---
+++ Rybinski_Ally_DIG3480_FinalProject/Assets/Scripts/AlRyAnnounceText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlRyAnnounceText : MonoBehaviour {
+
+	
+	void Start () {
+        Destroy(gameObject, 2);
+        }
+	
+	
+	void Update () {
+		
+	}
+}
---
+++ Rybinski_Ally_DIG3480_FinalProject/Assets/Scripts/AlRyCharacterAnim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlRyCharacterAnim : MonoBehaviour {
+
+private Animator anim;
+
+void Start() {
+	anim = GetComponent<Animator>();
+}
+
+void Update() {
+	if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow)){
+		anim.SetBool("isWalking", true);
+	} else {
+		anim.SetBool("isWalking", false);
+	}
+}
+}
